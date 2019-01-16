2712fd0b2f760a4b46a9688362156f23dab1b2a8
Pirated Virus <saurabhk201@gmail.com>
2019-01-16 12:31:25 +0530

    Updated JS
    Comments for future ref

--- dom_fetcher.js
+++ dom_fetcher.js
@@ -1,5 +1,5 @@
 
-    // Famous script for srawling
+    // Famous script for crawling
     $foot = $('.feedItem :nth-child(3)'); $child = $($foot).find('a.article-detail');
 
     $high = $('.feedItem :nth-child(2)'); $content = $($high).find('span.content');
@@ -8,5 +8,5 @@
 
                     for ($i = 0; $i < $content.length; $i++) {
         console.log("<p class=\"cont\">" + $content[$i].textContent + "</p><br> <a src=\" " + $link[$i].dataset.articleUrl + "\">" + $child[$i].text + "</a><br>");
-
+        // For  future ref: Just Copy and paste output of console to VS Code, manipulate few arrangments, Save as HTML n then convert webpage into PDF. 
     }
