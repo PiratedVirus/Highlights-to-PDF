d6e3953a16f3e5e22cde016dd9be0436507cc762
Saurabh Kulkarni <saurabhk201@gmail.com>
2021-01-18 16:41:55 +0530

    Update hide_solutions.js

--- hide_solutions.js
+++ hide_solutions.js
@@ -1,5 +1,6 @@
 // To hide MadeEasy correct solutions
 $(".correctoption").removeClass("correctoption");
+$(".wrongoption").removeClass("wrongoption");
 
 // To hide Ravindra Babu's solutions
 $("tr:nth-child(3) p img").css('display', 'none'); 
